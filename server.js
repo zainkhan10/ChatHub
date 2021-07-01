3b36382ad3b97dc00959cbed5f0927b6e5f40490
Zain Khan <zain.khaan147@gmail.com>
2021-07-01 15:47:38 +0500

    socket connect works

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require("express");
 const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 5001 || process.env.PORT;
+io.on("connection", (socket) => {
+  console.log("new WS connection");
+  // Welcome current user
+  socket.emit("message", "Hello from socket");
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+  // Runs when client disconnect
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left that chat");
+  });
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
